f5286df75639ef480d589c7f099186e0be61193d
PBDm <pbdm915@gmail.com>
2014-05-06 01:04:23 +0800

    add use strict mode for js

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,18 @@
-//Affix for posts list sidebar
-var posts = $(".post-list-container");
-var posts_top = $(".post-list").offset().top;
-var posts_width = posts.width();
-window.onscroll = function(){
-	if (jQuery(window).scrollTop() > posts_top) {
-		//console.log('here');
-		posts.addClass("fixed")
-		posts.css("width",posts_width);
-	} else {
-		posts.removeClass("fixed")
-		posts.removeAttr("style");
+"use strict";
+(function($) {
+	//Affix for posts list sidebar
+	if ($("#blog").length > 0) {
+		var posts_top = $(".post-list").offset().top;
+		var posts = $(".post-list-container");
+		var posts_width = posts.width();
+		window.onscroll = function(){
+			if ($(window).scrollTop() > posts_top) {
+				posts.addClass("fixed")
+				posts.css("width",posts_width);
+			} else {
+				posts.removeClass("fixed")
+				posts.removeAttr("style");
+			}
+		}
 	}
-}
+}(jQuery));
