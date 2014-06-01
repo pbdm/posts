379cc97220607bd25af05702f110d8146c9687d4
PBDm <pbdm915@gmail.com>
2014-06-01 17:34:45 +0800

    add heads

--- js/custom.js
+++ js/custom.js
@@ -2,17 +2,70 @@
 (function($) {
 	//Affix for posts list sidebar
 	if ($(".post").length > 0) {
-		var posts_top = $(".post-list").offset().top - 10;
+		var postsTop = $(".post-list").offset().top - 10;
 		var posts = $(".post-list-container");
-		var posts_width = posts.width();
+		var postsWidth = posts.width();
 		window.onscroll = function(){
-			if ($(window).scrollTop() > posts_top) {
+			if ($(window).scrollTop() > postsTop) {
 				posts.addClass("fixed")
-				posts.css("width",posts_width);
+				posts.css("width",postsWidth);
 			} else {
 				posts.removeClass("fixed")
 				posts.removeAttr("style");
 			}
 		}
 	}
+	//head-animation
+	var hideHead = function() {
+		$("#head-animation > div").each(function(){
+			$(this).hide();
+		});
+	}
+	var headAnimation = $("#head-animation")[0];
+	var headTop = headAnimation.clientHeight/3+headAnimation.offsetTop;
+	var headBottom = headAnimation.clientHeight*2/3+headAnimation.offsetTop;
+	var headLeft = headAnimation.clientWidth/3+headAnimation.offsetLeft;
+	var headRight = headAnimation.clientWidth*2/3+headAnimation.offsetLeft;
+	var myX,myY;
+	hideHead();
+	$("#head-animation .center-center").show();
+	$(document).mousemove(function(e){
+	  hideHead();
+	  if (e.clientY < headTop) {
+	  	if (e.clientX < headLeft) {
+		  	$("#head-animation .top-left").show();
+		  	console.log('top-left');
+		  } else if (e.clientX < headRight){
+		  	$("#head-animation .top-center").show();
+		  	console.log('top-cente');
+		  } else {
+		  	$("#head-animation .top-right").show();
+		  	console.log('top-right');
+		  }
+	  } else if (e.clientY < headBottom){
+		  if (e.clientX < headLeft) {
+		  	$("#head-animation .center-left").show();
+		  	console.log('center-left');
+		  } else if (e.clientX < headRight){
+		  	$("#head-animation .center-center").show();
+		  	console.log('center center');
+		  } else {
+		  	$("#head-animation .center-right").show();
+		  	console.log('center-right');
+		  }
+	  } else {
+		  if (e.clientX < headLeft) {
+		  	$("#head-animation .bottom-left").show();
+		  	console.log('bottom-left');
+		  } else if (e.clientX < headRight){
+		  	$("#head-animation .bottom-center").show();
+		  	console.log('bottom-center');
+		  } else {
+		  	$("#head-animation .bottom-right").show();
+		  	console.log('bottom-right');
+		  }
+	  }
+	});
+	//hideHead();
+	
 }(jQuery));
