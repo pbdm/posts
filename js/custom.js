fa5fd167d3dadeb3e0af422d2ee745be6e2c227b
PBDm <pbdm915@gmail.com>
2014-06-10 23:36:54 +0800

    add transform for head

--- js/custom.js
+++ js/custom.js
@@ -3,15 +3,15 @@
 	//Affix for posts list sidebar
 	if ($(".post").length > 0) {
 		var postsTop = $(".post-list").offset().top - 10;
-		var posts = $(".post-list-container");
-		var postsWidth = posts.width();
+		var $posts = $(".post-list-container");
+		var postsWidth = $posts.width();
 		window.onscroll = function(){
 			if ($(window).scrollTop() > postsTop) {
-				posts.addClass("fixed")
-				posts.css("width",postsWidth);
+				$posts.addClass("fixed")
+				$posts.css("width",postsWidth);
 			} else {
-				posts.removeClass("fixed")
-				posts.removeAttr("style");
+				$posts.removeClass("fixed")
+				$posts.removeAttr("style");
 			}
 		}
 	}
@@ -21,51 +21,53 @@
 			$(this).hide();
 		});
 	}
-	var headAnimation = $("#head-animation")[0];
-	var headTop = headAnimation.offsetTop;
-	var headBottom = headAnimation.clientHeight+headAnimation.offsetTop;
-	var headLeft = headAnimation.offsetLeft;
-	var headRight = headAnimation.clientWidth+headAnimation.offsetLeft;
+	var $headAnimations = $("#head-animation");
+	var $headAnimation = $headAnimations[0];
+	var headTop = $headAnimation.offsetTop;
+	var headMid = $headAnimation.offsetTop+$headAnimation.clientHeight/2;
+	var headBottom = $headAnimation.clientHeight+$headAnimation.offsetTop;
+	var headLeft = $headAnimation.offsetLeft;
+	var headCenter = $headAnimation.offsetLeft+$headAnimation.clientWidth/2;
+	var headRight = $headAnimation.clientWidth+$headAnimation.offsetLeft;
 	var myX,myY;
+	var Ratio = -50;
 	hideHead();
 	$("#head-animation .center-center").show();
 	$(document).mousemove(function(e){
+		myX = (e.clientX - headCenter)/Ratio;
+		myY = (e.clientY - headMid)/Ratio;
+		$headAnimations.css({
+			"transform"        : "translate(" + myX + "px," + myY + "px)",
+			"-webkit-transform": "translate(" + myX + "px," + myY + "px)",
+			"-ms-transform"    : "translate(" + myX + "px," + myY + "px)",
+			"-moz-transform"   : "translate(" + myX + "px," + myY + "px)",
+			"-o-transform"     : "translate(" + myX + "px," + myY + "px)",
+		});
 	  hideHead();
 	  if (e.clientY < headTop) {
 	  	if (e.clientX < headLeft) {
 		  	$("#head-animation .top-left").show();
-		  	console.log('top-left');
 		  } else if (e.clientX < headRight){
 		  	$("#head-animation .top-center").show();
-		  	console.log('top-cente');
 		  } else {
 		  	$("#head-animation .top-right").show();
-		  	console.log('top-right');
 		  }
 	  } else if (e.clientY < headBottom){
 		  if (e.clientX < headLeft) {
 		  	$("#head-animation .center-left").show();
-		  	console.log('center-left');
 		  } else if (e.clientX < headRight){
 		  	$("#head-animation .center-center").show();
-		  	console.log('center center');
 		  } else {
 		  	$("#head-animation .center-right").show();
-		  	console.log('center-right');
 		  }
 	  } else {
 		  if (e.clientX < headLeft) {
 		  	$("#head-animation .bottom-left").show();
-		  	console.log('bottom-left');
 		  } else if (e.clientX < headRight){
 		  	$("#head-animation .bottom-center").show();
-		  	console.log('bottom-center');
 		  } else {
 		  	$("#head-animation .bottom-right").show();
-		  	console.log('bottom-right');
 		  }
 	  }
 	});
-	//hideHead();
-	
 }(jQuery));
