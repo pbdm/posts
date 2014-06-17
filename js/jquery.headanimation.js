2bbf7990207eca3f2b0ad63faead9d245d6aa930
PBDm <pbdm915@gmail.com>
2014-06-17 23:22:23 +0800

    delete parent selector in jquery.headanimations.js

--- js/jquery.headanimation.js
+++ js/jquery.headanimation.js
@@ -1,67 +1,77 @@
 ;(function ($) {
   "use strict";
-  $.fn.headAnimation = function(options) {
-    var 
-      $this = this,
-      settings = $.extend({
-        'ratio': 50
-      }, options);
-    
-    function hideHead() {
-      $("#head-animation > div").each(function() {
-        $(this).hide();
-      });
-    }
-
-    return this.each(function () {
-      var
-        headTop    = this.offsetTop,
-        headMid    = this.offsetTop + this.clientHeight/2,
-        headBottom = this.clientHeight + this.offsetTop,
-        headLeft   = this.offsetLeft,
-        headCenter = this.offsetLeft + this.clientWidth/2,
-        headRight  = this.clientWidth + this.offsetLeft,
-        myX,
-        myY;
-      hideHead();
-      $("#head-animation .center-center").show();
-      $(document).mousemove(function(e) {
-        myX = (e.clientX - headCenter) / settings.ratio;
-        myY = (e.clientY - headMid) / settings.ratio;
-        $this.css({
-          "transform"         : "translate(" + myX + "px," + myY + "px)",
-          "-webkit-transform" : "translate(" + myX + "px," + myY + "px)",
-          "-ms-transform"     : "translate(" + myX + "px," + myY + "px)",
-          "-moz-transform"    : "translate(" + myX + "px," + myY + "px)",
-          "-o-transform"      : "translate(" + myX + "px," + myY + "px)",
-        });
-        hideHead();
-        if (e.clientY < headTop) {
-          if (e.clientX < headLeft) {
-            $("#head-animation .top-left").show();
-          } else if (e.clientX < headRight) {
-            $("#head-animation .top-center").show();
-          } else {
-            $("#head-animation .top-right").show();
-          }
-        } else if (e.clientY < headBottom) {
-          if (e.clientX < headLeft) {
-            $("#head-animation .center-left").show();
-          } else if (e.clientX < headRight) {
-            $("#head-animation .center-center").show();
-          } else {
-            $("#head-animation .center-right").show();
-          }
-        } else {
-          if (e.clientX < headLeft) {
-            $("#head-animation .bottom-left").show();
-          } else if (e.clientX < headRight) {
-            $("#head-animation .bottom-center").show();
-          } else {
-            $("#head-animation .bottom-right").show();
-          }
-        }
-      });
+  function hideHead(arg) {
+    arg.children("div").each(function() {
+      $(this).hide();
     });
+  }
+  var methods = {
+    init: function(options) {
+      var $this = this;
+      return this.each(function () {
+        var
+          headTop    = this.offsetTop,
+          headMid    = this.offsetTop + this.clientHeight/2,
+          headBottom = this.clientHeight + this.offsetTop,
+          headLeft   = this.offsetLeft,
+          headCenter = this.offsetLeft + this.clientWidth/2,
+          headRight  = this.clientWidth + this.offsetLeft,
+          myX,
+          myY,
+          defaults = {
+            'ratio': 50
+          },
+          settings = $.extend({}, defaults, options);
+        hideHead($this);
+        $this.children(".center-center").show();
+        $(document).mousemove(function(e) {
+          myX = (e.clientX - headCenter) / settings.ratio;
+          myY = (e.clientY - headMid) / settings.ratio;
+          $this.css({
+            "transform"         : "translate(" + myX + "px," + myY + "px)",
+            "-webkit-transform" : "translate(" + myX + "px," + myY + "px)",
+            "-ms-transform"     : "translate(" + myX + "px," + myY + "px)",
+            "-moz-transform"    : "translate(" + myX + "px," + myY + "px)",
+            "-o-transform"      : "translate(" + myX + "px," + myY + "px)",
+          });
+          hideHead($this);
+          if (e.clientY < headTop) {
+            if (e.clientX < headLeft) {
+              $this.children(".top-left").show();
+            } else if (e.clientX < headRight) {
+              $this.children(".top-center").show();
+            } else {
+              $this.children(".top-right").show();
+            }
+          } else if (e.clientY < headBottom) {
+            if (e.clientX < headLeft) {
+              $this.children(".center-left").show();
+            } else if (e.clientX < headRight) {
+              $this.children(".center-center").show();
+            } else {
+              $this.children(".center-right").show();
+            }
+          } else {
+            if (e.clientX < headLeft) {
+              $this.children(".bottom-left").show();
+            } else if (e.clientX < headRight) {
+              $this.children(".bottom-center").show();
+            } else {
+              $this.children(".bottom-right").show();
+            }
+          }
+        });
+      });
+    },
+  };
+  $.fn.headAnimation = function(options) {
+    var method = arguments[0];
+    if(methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === "object" || !method ) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.headanimation');
+    }
   }
 })(jQuery);
