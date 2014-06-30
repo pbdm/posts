15cfe019116bd5bad3bdce0d29c9ca8aa3511bf1
PBDm <pbdm915@gmail.com>
2014-06-30 23:40:53 +0800

    add scroll watcher for toc

--- js/jquery.toc.js
+++ js/jquery.toc.js
@@ -8,6 +8,9 @@
         $c,
         $a,
         $li,
+        scrollTop = [],
+        top,
+        index,
         $ul = $('<ul/>').attr("id", "toc");
 
       this.children(":header").each(function(){       
@@ -17,8 +20,23 @@
         $a = $('<a/>').attr('href', "#" + $c).text($c);
         $li = $('<li/>').addClass("toc-" + $t).append($a);
         $ul.append($li);
+        scrollTop.push($(this).offset().top);
       });
       $(".list-container").prepend($ul);
+      length = scrollTop.length;
+      $(window).scroll(function(){
+        top = $(window).scrollTop() + 50;
+        for(var i=0;i<length;i++){
+          if(top <= scrollTop[i]){
+            index = i;
+            break;
+          }
+        }
+        $('#toc li').removeClass('on');
+        if(top >= scrollTop[0]){
+          $('#toc li').eq(index - 1).addClass('on');
+        }
+      });
     },
   };
   $.fn.toc = function(options) {
