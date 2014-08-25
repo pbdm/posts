b595917a2c774893a196925db306ca1a8ae13c8f
PBDm <pbdm915@gmail.com>
2014-08-25 22:15:19 +0800

    add responsive menu

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,17 @@ affix();
 // navtop
 // whichBrowser().ie ? '' : navTop();
 
+//responsive menu
+var navList = document.querySelector("nav .wrapper > ul");
+var navMenu = document.getElementsByClassName("fa-list-ul")[0];
+navMenu.addEventListener ('click',function() {
+  if (getComputedStyle(navList)['height'] == "1px") {
+    navList.style.height = "auto";
+  } else {
+    navList.style.height = "1px";
+  }
+});
+
 (function($) {
 	// head-animation
 	// $("#head-animation").headAnimation({
@@ -23,7 +34,7 @@ affix();
 
   // back to top
   var $btt = $('#back-to-top');
-  $(window).scroll(function(){
+  $(window).scroll(function() {
     if ($(this).scrollTop() > 50) {
       $btt.css("opacity", 1);
     } else {
