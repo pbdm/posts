69fe6df2c3da0d4a5f1142a7c700fec2043fbc74
PBDm <pbdm915@gmail.com>
2016-03-07 11:29:38 +0800

    update

--- posts/wiki/2014-05-14-Javascript.md
+++ posts/wiki/2014-05-14-Javascript.md
@@ -1,9 +1,9 @@
 * Firefox获取body元素和Chrome有所不同 `root = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body
 `
 * [JavaScript获取DOM元素位置和尺寸大小](http://www.cnblogs.com/dolphinX/archive/2012/11/19/2777756.html)
-* `splice()` 方法与 `slice()` 方法的作用是不同的，splice() 方法会直接对数组进行修改
-`splice()` 返回被删除元素
+* `splice()` 方法与 `slice()` 方法的作用是不同的，`splice()` 方法会直接对数组进行修改,并返回被删除元素, `slice()`不改变原数组
 * `for in` 会把原型里的东西都遍历出来,只有用`hasOwnProperty`多判断一遍, `for`就不会了,还是多用for吧...
+* 复制数组:对于webkit, 使用`concat`; 其他浏览器, 使用`slice`
 
 ## ES6
 for循环头部的let声明会有一个特殊的行为，这个行为指出变量在循环过程中不止被声明一次，每次迭代都会声明。 随后的每个迭代都会使用上一个迭代结束时的值在初始化这个变量
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,8 @@
-var path = require('path');
 var autoprefixer = require('autoprefixer');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var fs = require('fs');
 var webpack = require('webpack');
-
-var tree = function(filepath) {
-
-  filepath = path.normalize(filepath);
-
-  var result = [];
-
-  var files = fs.readdirSync(filepath);
-
-  for (key in files) {
-    file = files[key];
-    if (files[key].substr(-2,2) === 'md') {
-      result.push ({
-        date:  files[key].substr(0,10),
-        fullpath:  encodeURIComponent(filepath + '/' + files[key]),
-        path: encodeURIComponent(files[key].slice(11,-3).toLowerCase()),
-        title: files[key].slice(11,-3)
-      });
-    }
-  }
-
-  return result;
-}
-var posts = {
-  wiki: tree('./posts/wiki'),
-  blog: tree('./posts/blog')
-};
+var fileListPlugin = require('filelist-json-webpack-plugin');
 
 module.exports = {
   entry: { 
@@ -60,13 +32,20 @@ module.exports = {
       //verbose: true, 
       //dry: false
     //}),
+    new fileListPlugin({
+      key: 'posts',
+      paths: {
+        wiki: './posts/wiki',
+        blog: './posts/blog'
+      }
+    }),
     new HtmlWebpackPlugin({
       template: 'tmpl/index.html',
       filename: 'index.html',
       hash: true
     }),
-    new webpack.DefinePlugin({
-      posts: JSON.stringify(posts)
-    })
+    //new webpack.DefinePlugin({
+      //posts: JSON.stringify(posts)
+    //})
   ]
 };
